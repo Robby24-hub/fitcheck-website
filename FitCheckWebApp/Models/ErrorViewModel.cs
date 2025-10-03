9e50a4e1e9dee98e9a38d08557b3954c986f99f3
Mult0 <taezarex@gmail.com>
2025-10-04 00:52:51 +0800

    Main Web App

---
+++ FitCheckWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitCheckWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
