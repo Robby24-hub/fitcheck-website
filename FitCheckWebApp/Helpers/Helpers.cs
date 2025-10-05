d830c4ba0d68cb3ed2399b4e03f343aabd82a147
Mult0 <taezarex@gmail.com>
2025-10-06 03:22:33 +0800

    BCrpyt hashing for passwords.

--- FitCheckWebApp/Helpers/Helpers.cs
+++ FitCheckWebApp/Helpers/Helpers.cs
@@ -24,10 +24,11 @@ namespace FitCheckWebApp.Helpers
 
         public static string? HashingPassword(string password)
         {
-            using var sha256 = System.Security.Cryptography.SHA256.Create();
-            var bytes = System.Text.Encoding.UTF8.GetBytes(password);
-            var hash = sha256.ComputeHash(bytes);
-            return Convert.ToBase64String(hash);
+            
+            string hash = BCrypt.Net.BCrypt.EnhancedHashPassword(password, 13);
+
+            return hash;
+
         }
 
 
