688abefb8eb699b5ed3bab1ccec7929e557cf446
Mult0 <taezarex@gmail.com>
2025-10-25 09:41:38 +0800

    Functioning forgot password and change password

--- FitCheckWebApp/Services/EmailWarningService.cs
+++ FitCheckWebApp/Services/EmailWarningService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using FitCheckWebApp.DataAccess;
+﻿using FitCheckWebApp.DataAccess;
 using FitCheckWebApp.Helpers;
 
 namespace FitCheckWebApp.Services
